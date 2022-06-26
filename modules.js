c953d7f071b25c23d8405f054cf05436f28ded37
Provi0880 <thierryprovidence@gmail.com>
2022-06-26 08:19:37 +0000

    first commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind')
+
+
+sayHi('susan')
+sayHi('john')
+sayHi('peter')
